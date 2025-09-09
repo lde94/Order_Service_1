66d88e698c4602790103e3e8d0a9eab7c40526da
Alexander <97alegre@gafe.molndal.se>
2025-09-09 09:12:57 +0200

    Initial commit

---
+++ src/main/java/se/order_service_1/OrderService1Application.java
@@ -0,0 +1,13 @@
+package se.order_service_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderService1Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/order_service_1/OrderService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.order_service_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
