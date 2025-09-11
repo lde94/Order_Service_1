b8b48c90644b84d102ca0687222d5165b5d8fab5
Erik <erik.thorsell.95@gmail.com>
2025-09-11 10:34:22 +0200

    Added swagger operations

--- src/main/java/se/order_service_1/controller/OrderController.java
+++ src/main/java/se/order_service_1/controller/OrderController.java
@@ -1,5 +1,6 @@
 package se.order_service_1.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +20,14 @@ import java.util.List;
 @AllArgsConstructor
 public class OrderController {
     private OrderService orderService;
-
+    @Operation(summary = "Get order by id", description = "Get a list of products for a specific order by id")
     @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponse> getOrderById(@PathVariable Long orderId) {
         Order order = orderService.getOrderById(orderId);
         OrderResponse orderResponse = createOrderResponse(order);
         return ResponseEntity.ok(orderResponse);
     }
-
+    @Operation(summary = "Get order history", description = "Get order history for a specific user by id")
     @GetMapping("/orderHistory/{userId}")
     public ResponseEntity<List<OrderResponse>> getOrderHistory(@PathVariable Long userId) {
         List<Order> orderList = orderService.getOrdersByUser(userId);
@@ -47,26 +48,26 @@ public class OrderController {
         OrderResponse orderResponse = createOrderResponse(order);
         return ResponseEntity.ok(orderResponse);
     }
-
+    @Operation(summary = "Add item to order", description = "Add item to order with id and quantity")
     @PostMapping("/addToOrder")
     public ResponseEntity<OrderResponse> addToOrder(@RequestBody OrderRequest orderRequest) {
         orderService.addOrderItem(orderRequest.getOrderId(), orderRequest.getProductId(), orderRequest.getQuantity());
         Order order = orderService.getOrderById(orderRequest.getOrderId());
         return ResponseEntity.status(HttpStatus.CREATED).body(createOrderResponse(order));
     }
-
+    @Operation(summary = "Finalize order", description = "Finalize ongoing order by orderId")
     @PutMapping("/finalizeOrder/{orderId}")
     public ResponseEntity<String> finalizeOrder(@PathVariable Long orderId) {
         orderService.finalizeOrder(orderId);
         return ResponseEntity.ok("Order has been finalized");
     }
-
+    @Operation(summary = "Update order", description = "Update order with productId and quantity")
     @PutMapping("/update")
     public ResponseEntity<OrderResponse> updateOrder(@RequestBody OrderRequest orderRequest) {
         Order order = orderService.updateOrder(orderRequest.getOrderId(), orderRequest.getProductId(), orderRequest.getQuantity());
         return ResponseEntity.ok(createOrderResponse(order));
     }
-
+    @Operation(summary = "Delete order", description = "Cancel order with orderId")
     @DeleteMapping("/cancel/{orderId}")
     public ResponseEntity<String> cancelOrder(@PathVariable Long orderId) {
         orderService.deleteOrder(orderId);
